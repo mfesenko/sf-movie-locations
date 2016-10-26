1f3eafce25b5ef91c0dc9110f0282446a81524bb
Mary Fesenko <m.fesenko17@gmail.com>
2016-10-27 00:53:34 +0300

    Changed json name of Longitude field

--- persistence/models.go
+++ persistence/models.go
@@ -5,7 +5,7 @@ import "gopkg.in/mgo.v2/bson"
 type (
 	Location struct {
 		Latitude  float64 `json:"lat"`
-		Longitude float64 `json:"long"`
+		Longitude float64 `json:"lng"`
 	}
 
 	Movie struct {
